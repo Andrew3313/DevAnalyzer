d1b514e5dc60753c9c351451920755b808d2dd4b
Andrew <andystep333@gmail.com>
2026-03-16 01:58:35 +0300

    feat: add login schema

---
+++ src/features/auth/model/login.schema.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod'
+
+export const LoginSchema = z.object({
+	email: z.email('Невалидный email'),
+	password: z.string().min(1, {
+		message: 'Обязательное поле'
+	})
+})
+
+export type TLoginSchema = z.infer<typeof LoginSchema>
