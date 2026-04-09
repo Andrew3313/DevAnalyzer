94f9c8418707bb68227374eba77056736fef3a9b
Andrew <andystep333@gmail.com>
2026-04-09 17:55:00 +0300

    feat: add basic statistics types

---
+++ src/entities/candidate-stats/model/types.ts
@@ -0,0 +1,22 @@
+export interface IHeatmapWeek {
+	weekStart: string
+	days: number[]
+	total: number
+}
+
+export type IHeatmapWeekList = IHeatmapWeek[]
+
+export interface ICandidateStats {
+	githubId: number
+	login: string
+	name: string | null
+	location: string | null
+	company: string | null
+	repositories: number
+	stars: number
+	forks: number
+	followers: number
+	commits: number
+	ageInDays: number
+	heatmap: IHeatmapWeekList
+}
